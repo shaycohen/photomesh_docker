f2fe9cf746e83ec47f59a8ba69d0f4ae7ba621a3
LiranGSR <132265350+LiranGSR@users.noreply.github.com>
2025-10-13 12:10:31 +0300

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,20 +1,22 @@
 #!/usr/bin/env bash
-IP="${ENV_IP-192.168.1.1}"
-DIR="${ENV_DIR-data_directory}"
-SHAREFOLDER="${ENV_SHAREFOLDER-\\\\$IP\\c\\temp\\}"
+NAS_IP="${NAS-IP-192.168.1.1}"
+NAS_NAME="${NAS-NAME-vs-fn-nfs}"
+SHARE_NAME="${SHARE_NAME-data_directory}"
+SHAREFOLDER="${\\\\$NAS_IP\\$SHARE_NAME\\working-folder\\}"
 ACTION="${1:-start}"
-echo "IP[$IP] DIR[$DIR] SHAREFOLDER[$SHAREFOLDER] ACTION[$ACTION]"
+echo "NASIP[$NAS_IP] NASNAME[$NAS_NAME] SHARENAME[$SHARE_NAME] SHAREFOLDER[$SHAREFOLDER] ACTION[$ACTION]"
 
-mkdir -p ~/.wine/dosdevices/unc/${ENV_IP}
-mkdir -p ~/.wine/dosdevices/unc/${ENV_DIR}
-ln -sf /data ~/.wine/dosdevices/unc/${ENV_DIR}/d
+mkdir -p ~/.wine/dosdevices/unc/${NAS_IP}
+mkdir -p ~/.wine/dosdevices/unc/${NAS_NAME}
+ln -sf /data ~/.wine/dosdevices/unc/${NAS_IP}/${SHARE_NAME}
+ln -sf /data ~/.wine/dosdevices/unc/${NAS_NAME}/${SHARE_NAME}
 mkdir -p ~/.wine/drive_c/users/ubuntu/AppData/Roaming/PhotomeshFuser
 echo -n "$SHAREFOLDER" > ~/.wine/drive_c/users/ubuntu/AppData/Roaming/PhotomeshFuser/ShareFolder.txt
 echo "Content of ~/.wine/drive_c/users/ubuntu/AppData/Roaming/PhotomeshFuser/ShareFolder.txt :"
 cat ~/.wine/drive_c/users/ubuntu/AppData/Roaming/PhotomeshFuser/ShareFolder.txt
 echo
 echo "Created folders:"
-ls -ld ~/.wine/dosdevices/unc/${ENV_IP} ~/.wine/dosdevices/unc/${ENV_DIR} /data ~/.wine/drive_c/users/ubuntu/AppData/Roaming/PhotomeshFuser
+ls -ld ~/.wine/dosdevices/unc/${NAS_IP} ~/.wine/dosdevices/unc/${NAS_NAME} ~/.wine/dosdevices/unc/${NAS_IP}/${SHARE_NAME} /data ~/.wine/drive_c/users/ubuntu/AppData/Roaming/PhotomeshFuser
 
 [[ $ACTION == "start" ]] && { 
 	sudo Xvfb :0 -screen 0 1024x768x24 &
